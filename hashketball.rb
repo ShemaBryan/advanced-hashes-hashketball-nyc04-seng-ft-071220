1d9bb3dbb2f427bc52f75e080f813c0e78427cb6
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-29 14:02:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,7 +182,19 @@ def player_numbers(team_name)
   end
 nums
 end
-def player_stats(player_name)
+
+def player_stats(player_name, hashketball)
+  player_name.capitalize!
+  if hashketball[:home][:players].include?(player_name)
+    hashketball[:home][:players][player_name][:stats]
+  elsif hashketball[:away][:players].include?(player_name)
+    hashketball[:away][:players][player_name][:stats]
+  else
+    "No player found."
+  end
+end
+
+
 def big_shoe_rebounds
   biggest_shoe = 0
   num_rebounds = 0
